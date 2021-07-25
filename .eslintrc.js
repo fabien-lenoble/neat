3e7696cfd78daf62d5af18b5229c7b7fa3bf3e20
fabien-lenoble <fabien.lenoble1@gmail.com>
2021-07-25 21:10:58 +0200

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,11 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:vue/essential",
+    "plugin:vue/recommended",
     "eslint:recommended",
     "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
+    // "@vue/prettier",
+    // "@vue/prettier/@typescript-eslint",
   ],
   parserOptions: {
     ecmaVersion: 2020,
@@ -16,5 +16,10 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'semi': 'error',
+    'indent': ['error', 4],
+    'no-trailing-spaces': 'error',
+    'vue/script-indent': ['error', 4, { baseIndent: 0 }],
+    'vue/html-indent': ['error', 4, { baseIndent: 1 }],
   },
 };
